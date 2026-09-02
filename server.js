17f92c109f6491d2ec546cda3d9e009332b21696
Padmaja Dhulipudi <163881255+Padmaja-Dhulipudi@users.noreply.github.com>
2026-09-02 22:03:38 +0530

    Refactor server.js to remove CORS setup
    Removed CORS middleware and updated socket.io initialization.

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors());
+const io = new Server(server);
 
-const io = new Server(server, {
-    cors: {
-        origin: "*"
-    }
-});
+app.use(express.static(path.join(__dirname, "public")));
 
 let votes = {
     Java: 0,
